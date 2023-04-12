72a2756db12ffe5b14405cb2d919cef3c4228227
NACHIKET PRAJAPATI <119723524+Nachiket-2005@users.noreply.github.com>
2023-04-12 19:00:17 +0530

    Arrays

---
+++ ARRAYS/kadanesAlgorithm.java
@@ -0,0 +1,22 @@
+package ARRAYS;
+
+public class kadanesAlgorithm {
+    public static void Kadanes(int numbers[]){
+        int ms = Integer.MIN_VALUE;
+        int cs = 0;
+
+        for(int i=0;i<numbers.length;i++){
+            cs = cs + numbers[i];
+            if(cs < 0){
+                cs = 0;
+            }
+            ms = Math.max(cs , ms);
+        }
+        System.out.println("Our max subarray sum is = "+ms);
+    }
+
+    public static void main(String args[]){
+        int numbers[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+        Kadanes(numbers);
+    }
+}
