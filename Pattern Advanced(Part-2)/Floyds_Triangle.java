37c1f92d91d678f4593412ba013f3b00eeb176a4
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2023-09-18 22:26:16 +0530

    Prac

---
+++ Pattern Advanced(Part-2)/Floyds_Triangle.java
@@ -0,0 +1,18 @@
+public class Floyds_Triangle {
+
+    public static void floyds_Triangle(int n) {
+        int counter = 1;
+        //outer loop
+        for(int i=1; i<=n; i++) {
+            //inner loop -> counter print
+            for(int j=1; j<=i; j++) {
+                System.out.print(counter+" ");
+                counter++;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]) {
+        floyds_Triangle(4);
+    }
+}
