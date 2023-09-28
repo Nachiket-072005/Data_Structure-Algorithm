4e6d41266b94203ffea8726651ecffca4abca8b2
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2023-09-28 17:20:36 +0530

    Arrays Proble

---
+++ 8.Arrays_DS_1/Pairs_In_An_Array.java
@@ -0,0 +1,20 @@
+public class Pairs_In_An_Array {
+
+    public static void printPairs(int numbers[]) {
+        int tp = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            int curr = numbers[i];
+            for (int j = i + 1; j < numbers.length; j++) {
+                System.out.print("(" + curr + "," + numbers[j] + ")");
+                tp++;
+            }
+            System.out.println();
+        }
+        System.out.println("Total pairs : " + tp);
+    }
+
+    public static void main(String args[]) {
+        int numbers[] = { 2, 4, 6, 8, 10 };
+        printPairs(numbers);
+    }
+}
