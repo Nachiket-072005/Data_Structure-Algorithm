95230a8f1acf58ef53026d3fe02b1f52ecb2e44b
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2023-09-30 07:12:11 +0530

    Arrays

---
+++ 8.Arrays_DS_1/Max_SubArrays_Sum.java
@@ -0,0 +1,33 @@
+public class Max_SubArrays_Sum {
+
+    public static void maxSubarraySum(int numbers[]) {
+        int maxSum = Integer.MIN_VALUE;
+        int minSum = Integer.MAX_VALUE;
+        int currSum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            int start = i;
+            for (int j = i; j < numbers.length; j++) {
+                int end = j;
+                currSum = 0;
+                for (int k = start; k <= end; k++) {
+                    currSum += numbers[k];
+                }
+                System.out.println("currSum = " + currSum);
+
+                if (maxSum < currSum) {
+                    maxSum = currSum;
+                }
+                if (minSum > currSum) {
+                    minSum = currSum;
+                }
+            }
+        }
+        System.out.println("Max Sum is = " + maxSum);
+        System.out.println("Min Sum is = " + minSum);
+    }
+
+    public static void main(String args[]) {
+        int numbers[] = { 2, 4, 6, 8, 10 };
+        maxSubarraySum(numbers);
+    }
+}
