1da02779a9c1d7978356eece883849a4e6410306
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-03-15 21:53:49 +0530

    Buy and Sell Stocks code

---
+++ DSA/8.Arrays_DS_1/Advanced_Problems/Buy_and_Sell_Stocks.java
@@ -0,0 +1,24 @@
+package Advanced_Problems;
+
+public class Buy_and_Sell_Stocks {
+    public static int buyAndSellStocks(int prices[]) {
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int i = 0; i<prices.length; i++) {
+            if(buyPrice < prices[i]) {
+                int profit = prices[i] - buyPrice; //today's profit
+                maxProfit = Math.max(maxProfit, profit);
+            }
+            else {
+                buyPrice = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+    public static void main(String args[]) {
+        int prices[] = {7, 1, 5, 3, 6, 4};
+
+        System.out.println(buyAndSellStocks(prices));
+    }
+}
