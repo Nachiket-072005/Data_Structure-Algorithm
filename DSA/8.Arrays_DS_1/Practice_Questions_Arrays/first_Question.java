6100580e8022ea3cf8f2528866f790dee6732478
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-03-15 22:17:31 +0530

    Practice Questions 1 To find element in an array if element exists twice in an array or not it will give true or false badsed on its choice of ourcode

---
+++ DSA/8.Arrays_DS_1/Practice_Questions_Arrays/first_Question.java
@@ -0,0 +1,20 @@
+package Practice_Questions_Arrays;
+
+public class first_Question {
+
+    public static boolean containsDuplicate(int nums[]) {
+        for(int i=0; i<nums.length-1; i++) {
+            for(int j=i+1; j<nums.length; j++) {
+                if(nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String args[]) {
+        int nums[] = {1, 2, 3, 1};
+
+        System.out.println(containsDuplicate(nums));
+    }
+}
