9efcfa10b1d9c02f0c278a8d16c970f1d0ab3d85
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-03-16 09:52:49 +0530

    Fourth Questions trapping rainwater now onwards one more questions remaining but I will find the solution of that question then upload

---
+++ DSA/8.Arrays_DS_1/Practice_Questions_Arrays/fourthQuestion.java
@@ -0,0 +1,26 @@
+public class fourthQuestion {
+    public static int trapped(int height[]) {
+        int n = height.length - 1;
+
+        int res = 0, l = 0, r = n - 1;
+        int lmax = height[l], rmax = height[r];
+
+        while (l < r) {
+            if (lmax < rmax) {
+                l++;
+                lmax = Math.max(lmax, height[l]);
+                res += lmax - height[l];
+            } else {
+                r--;
+                rmax = Math.max(rmax, height[r]);
+                res += rmax - height[r];
+            }
+        }
+        return res;
+    }
+
+    public static void main(String args[]) {
+        int height[] = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+        System.out.println(trapped(height));
+    }
+}
