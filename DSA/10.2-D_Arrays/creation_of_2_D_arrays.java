6b4cd0375b7d433bacc41818146636724485d127
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-03-24 08:06:33 +0530

    Creation of 2_D-Arrays & find max, min value in an array

---
+++ DSA/10.2-D_Arrays/creation_of_2_D_arrays.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+public class creation_of_2_D_arrays {
+
+    public static boolean search(int matrix[][], int key) {
+        for(int i=0; i<matrix.length; i++) {
+            for(int j=0; j<matrix[0].length; j++) {
+                if(matrix[i][j] == key) {
+                    System.out.println("Key is at index : " + "(" + i + "," + j + ")");
+                    return true;
+                }
+            }
+        }
+        System.out.println("Key is not found at index");
+        return false;
+    }
+
+    public static void largest(int matrix[][]) {
+        int largest = Integer.MIN_VALUE;
+        for(int i=0; i<matrix.length; i++) {
+            for(int j=0; j<matrix[0].length; j++) {
+                if(matrix[i][j] > largest) {
+                    largest = matrix[i][j];
+                }
+            }
+        }
+        System.out.println("largest element in the matrix is : " + largest);
+    }
+
+    public static void smallest(int matrix[][]) {
+        int smallest = Integer.MAX_VALUE;
+        for(int i=0; i<matrix.length; i++) {
+            for(int j=0; j<matrix[0].length; j++) {
+                if(matrix[i][j] < smallest) {
+                    smallest = matrix[i][j];
+                }
+            }
+        }
+        System.out.println("smallest element in the matrix is : " + smallest);
+    }
+    public static void main(String[] args) {
+        int matrix[][] = new int[3][3];
+        int n = matrix.length, m = matrix[0].length;
+        Scanner sc = new Scanner(System.in);
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        sc.close();
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        search(matrix, 4);
+        largest(matrix);
+        smallest(matrix);
+    }    
+}
