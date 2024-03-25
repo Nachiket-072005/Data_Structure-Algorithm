89592a5e7d97f9b51bf056ea9cb6548496f0ea5b
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-03-25 08:40:43 +0530

    declaration of Strings

---
+++ DSA/11.Strings/declaration_of_String.java
@@ -0,0 +1,11 @@
+public class declaration_of_String {
+    public static void main(String[] args) {
+        char arr[] = {'a', 'b', 'c', 'd'};
+        //Syntax 1:
+        String str1 = "abcd";
+        //Syntax 2:
+        String str2 = new String("xyz");
+
+        // NOTE THAT: Strings are IMMUTABLE.
+    }
+}
