74e3e6198eda92b42b64711998409fdcebee6222
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-03-25 09:07:09 +0530

    String charAt Method

---
+++ DSA/11.Strings/String_charAt_Method.java
@@ -0,0 +1,14 @@
+public class String_charAt_Method {
+
+    public static void printChar(String name) {
+        for(int i=0; i<name.length(); i++) {
+            System.out.print(name.charAt(i) + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        String name = "Nachiket";
+
+        printChar(name);
+    }
+}
