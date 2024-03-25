77095f6488be75fae5a9ddeb83623896663ba45c
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-03-25 08:56:36 +0530

    concatenation in Strings

---
+++ DSA/11.Strings/concatenation_in_String.java
@@ -0,0 +1,8 @@
+public class concatenation_in_String {
+    public static void main(String[] args) {
+        String firstName = "Nachiket";
+        String lastName = "Prajapati";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+    }
+}
