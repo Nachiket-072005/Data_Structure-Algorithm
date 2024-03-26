ad3c4578a85b9239fc34509658ce3b9a1864d673
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-03-26 08:40:28 +0530

    bitwise 1's complement

---
+++ DSA/12.Bit Manipulation/bitwise_1s_complement.java
@@ -0,0 +1,6 @@
+public class bitwise_1s_complement {
+    public static void main(String[] args) {
+        System.out.println(~5);
+        System.out.println(~0);
+    }
+}
---
+++ DSA/12.Bit Manipulation/bitwise_XOR.java
@@ -0,0 +1,5 @@
+public class bitwise_XOR {
+    public static void main(String[] args) {
+        System.out.println(5 ^ 6);
+    }
+}
