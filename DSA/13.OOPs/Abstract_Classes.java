600e648d913cdb7d9da37bfe07acf1d749bd1ea9
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-03-28 23:07:55 +0530

    Abstract Classes

---
+++ DSA/13.OOPs/Abstract_Classes.java
@@ -0,0 +1,59 @@
+public class Abstract_Classes {
+    public static void main(String[] args) {
+        // Horse h = new Horse();
+        // h.eat();
+        // h.walk();
+        // System.out.println(h.color);
+        // h.changeColor();
+        // System.out.println(h.color);
+        // Chicken c = new Chicken();
+        // c.eat();
+        // c.walk();
+        Mustang myHorse = new Mustang();
+    }
+}
+
+abstract class Animal {
+    String color;
+
+    Animal() {
+        // color = "brown";
+        System.out.println("Animal Constructor called");
+    }
+
+    void eat() {
+        System.out.println("Animal Eats");
+    }
+
+    abstract void walk();
+}
+
+class Horse extends Animal {
+    Horse() {
+        System.out.println("Horse Constructor called");
+    }
+
+    void changeColor() {
+        color = "Dark-Brown";
+    }
+
+    void walk() {
+        System.out.println("Walks on 4 legs");
+    }
+}
+
+class Mustang extends Horse {
+    Mustang() {
+        System.out.println("Mustange Constructor called");
+    }
+}
+
+class Chicken extends Animal {
+    void changeColor() {
+        color = "Yellow";
+    }
+
+    void walk() {
+        System.out.println("Walks on 2 legs");
+    }
+}
