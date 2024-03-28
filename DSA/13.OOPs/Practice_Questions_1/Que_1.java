dd33b6a489f24009c37c663c367e538dfac593f8
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-03-29 00:18:29 +0530

    Que1

---
+++ DSA/13.OOPs/Practice_Questions_1/Que_1.java
@@ -0,0 +1,13 @@
+public class Que_1 {
+    public static void main(String[] args) {
+        Student s = new Student();
+        //Fill here...
+        s.name = "aman";
+        System.out.println(s.name);
+    }
+}
+
+class Student {
+    String name;
+    int marks;
+}
