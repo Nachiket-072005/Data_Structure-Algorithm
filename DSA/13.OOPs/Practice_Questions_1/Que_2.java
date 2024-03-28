d850754bd822b8cc05d9068748616f064b0db174
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-03-29 00:22:55 +0530

    Que2

---
+++ DSA/13.OOPs/Practice_Questions_1/Que_2.java
@@ -0,0 +1,15 @@
+public class Que_2 {
+    public static void main(String[] args) {
+        
+    }
+}
+
+class Person {
+    String name;
+    int weight;
+}
+
+class Student extends Person {
+    int rollNo;
+    String schoolName;
+}
