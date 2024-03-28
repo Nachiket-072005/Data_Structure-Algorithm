bc5c0cd326bf47689ec3158774a229aa48941943
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-03-29 00:59:10 +0530

    Que_9

---
+++ DSA/13.OOPs/Practice_Questions_1/Que_9.java
@@ -0,0 +1,14 @@
+public class Que_9 {
+    public static void main(String[] args) {
+        Test t = new Test();
+        t.set_marks(98);
+        System.out.println(Test.marks);
+    }
+}
+
+class Test {
+    static int marks;
+    void set_marks(int marks) {
+        this.marks = marks;
+    }
+}
