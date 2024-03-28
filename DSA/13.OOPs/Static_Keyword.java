382245802c69c3336edf1ade50f5a5868464582d
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-03-28 23:59:14 +0530

    Static Keyword

---
+++ DSA/13.OOPs/Static_Keyword.java
@@ -0,0 +1,27 @@
+public class Static_Keyword {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Nachiket";
+        System.out.println(s1.name);
+        s1.schoolName = "JMV";
+        System.out.println(s1.schoolName);
+
+        Student s2 = new Student();
+        System.out.println(s2.schoolName);
+    }
+}
+
+class Student {
+    String name;
+    int rollNo;
+
+    static String schoolName;
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    String getName() {
+        return this.name;
+    }
+}
