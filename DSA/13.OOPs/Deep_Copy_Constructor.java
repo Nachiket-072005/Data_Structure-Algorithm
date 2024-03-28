0f4756b1efedf4d4ae4ec1134bd48d1c79a93a6f
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-03-28 18:42:04 +0530

    Deep Copy Constructor

---
+++ DSA/13.OOPs/Deep_Copy_Constructor.java
@@ -0,0 +1,52 @@
+public class Deep_Copy_Constructor {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Nachiket";
+        s1.rollNo = 39;
+        s1.password = "abcd";
+        s1.marks[0] = 100;
+        s1.marks[1] = 95;
+        s1.marks[2] = 89;
+
+        Student s2 = new Student(s1);
+        s2.password = "xyz";
+        s1.marks[2] = 100;
+
+        for(int i=0; i<3; i++) {
+            System.out.println(s2.marks[i]);   
+        }
+
+    }
+}
+
+class Student {
+    int rollNo;
+    String name;
+    String password;
+    int marks[];
+
+    //deep copy constructor
+    Student(Student s1) {
+        marks = new int[3];
+        this.name = s1.name;
+        this.rollNo = s1.rollNo;
+        for(int i=0; i<3; i++) {
+            this.marks[i] = s1.marks[i];
+        }
+    }
+
+    Student() {
+        marks = new int[3];
+        System.out.println("Constructor is called...");
+    }
+
+    Student(String name) {
+        marks = new int[3];
+        this.name = name;
+    }
+
+    Student(int rollNo) {
+        marks = new int[3];
+        this.rollNo = rollNo;
+    }
+}
