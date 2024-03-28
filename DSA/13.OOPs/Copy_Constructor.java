5539d8429fbeb6e735f08f24825da1a9e84927fe
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-03-28 18:27:31 +0530

    Copy Constructor

--- DSA/13.OOPs/Copy_Constructor.java
+++ DSA/13.OOPs/Copy_Constructor.java
@@ -1,44 +1,49 @@
 public class Copy_Constructor {
-        public static void main(String[] args) {
-            Student s1 = new Student( "Nachiket", 4);
-            Student s2 = new Student("Nachiket");
-            Student s3 = new Student(5);
-            System.out.println("Name : " + s2.name);
-            System.out.println("rollNo : " + s3.rollNo);
-            s1.marks[0] = 100;
-            s1.marks[1] = 95;
-            s1.marks[2] = 86;
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Nachiket";
+        s1.rollNo = 39;
+        s1.password = "abcd";
+        s1.marks[0] = 100;
+        s1.marks[1] = 95;
+        s1.marks[2] = 89;
 
-            Student s4 = new Student(s1);
-            s1.marks[2] = 100;
-            for(int i=0; i<3; i++) {
-                System.out.println(s4.marks[i]); 
-            }
+        Student s2 = new Student(s1);
+        s2.password = "xyz";
+        s1.marks[2] = 100;
+
+        for(int i=0; i<3; i++) {
+            System.out.println(s2.marks[i]);   
         }
+
     }
-    
-    class Student {
-        String name;
-        int rollNo;
-        String password;
-        int marks[];
+}
 
-        Student(Student s1) {
-            marks = new int[3];
-            this.name = s1.name;
-            this.rollNo = s1.rollNo;
-            this.marks = s1.marks;
-        }
+class Student {
+    int rollNo;
+    String name;
+    String password;
+    int marks[];
 
-        Student(String name, int rollNo) {
-            System.out.println("Constructor is called...");
-            this.name = name;
-            this.rollNo = rollNo;
-        }
-        Student(int rollNo) {
-            this.rollNo = rollNo;
-        }
-        Student(String name) {
-            this.name = name;
-        }
+    Student(Student s1) {
+        marks = new int[3];
+        this.name = s1.name;
+        this.rollNo = s1.rollNo;
+        this.marks = s1.marks;
     }
+
+    Student() {
+        marks = new int[3];
+        System.out.println("Constructor is called...");
+    }
+
+    Student(String name) {
+        marks = new int[3];
+        this.name = name;
+    }
+
+    Student(int rollNo) {
+        marks = new int[3];
+        this.rollNo = rollNo;
+    }
+}
