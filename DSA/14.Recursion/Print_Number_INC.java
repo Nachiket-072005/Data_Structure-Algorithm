dc1adda8d253ab5cfcf23fbebffd3be359d7857c
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-03-31 10:03:21 +0530

    Print Number in INCS Order

---
+++ DSA/14.Recursion/Print_Number_INC.java
@@ -0,0 +1,14 @@
+public class Print_Number_INC {
+    public static void printInc(int n) {
+        if(n == 1) {
+            System.out.print(n + " ");
+            return;
+        }
+        printInc(n - 1);
+        System.out.print(n + " ");
+    }
+    public static void main(String[] args) {
+        int n = 10;
+        printInc(n);
+    }
+}
