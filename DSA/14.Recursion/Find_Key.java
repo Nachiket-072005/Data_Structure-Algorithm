bac47c2ed80d723e6f85582cf2292cc0dfef8866
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-03-31 12:39:30 +0530

    First Occurance of an element index

---
+++ DSA/14.Recursion/Find_Key.java
@@ -0,0 +1,17 @@
+public class Find_Key {
+    public static int firstOccurance(int arr[], int key, int i) {
+        if (i == arr.length) {
+            return -1;
+        }
+
+        if (arr[i] == key) {
+            return i;
+        }
+        return firstOccurance(arr, key, i + 1);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 4, 5, 7, 9, 2, 1, 4, 3 };
+        System.out.println(firstOccurance(arr, 4, 0));
+    }
+}
