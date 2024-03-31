172b56604c693ec07d7688f69e141aabb75ca184
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-03-31 11:11:37 +0530

    Print Nth Fibonacci Number

---
+++ DSA/14.Recursion/Print_Fibonacci_Series.java
@@ -0,0 +1,15 @@
+public class Print_Fibonacci_Series {
+    public static int printFib(int n) {
+        if(n == 0 || n == 1) {
+            return n;
+        }
+        int fnm1 = printFib(n - 1);
+        int fnm2 = printFib(n - 2);
+        int fn = fnm1 + fnm2;
+        return fn;
+    }
+    public static void main(String[] args) {
+        int n = 26;
+        System.out.println(printFib(n));
+    }
+}
