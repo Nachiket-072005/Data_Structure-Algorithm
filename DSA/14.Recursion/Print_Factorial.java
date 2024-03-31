244530d73d1c852faeb2aece08633a5029976863
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-03-31 10:17:39 +0530

    Find Factorial of a given number n

---
+++ DSA/14.Recursion/Print_Factorial.java
@@ -0,0 +1,15 @@
+public class Print_Factorial {
+    public static int printFact(int n) {
+        if(n == 1 || n == 0) {
+            return 1;
+        }
+        else {
+            return n * printFact(n - 1);
+        }
+    }
+    public static void main(String[] args) {
+        int n = 7;
+        int res = printFact(n);
+        System.out.println("Factorial is : " + res);
+    }
+}
