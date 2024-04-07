362667ac792707d6246b8e09f52733952bb9036f
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-04-07 18:57:18 +0530

    Revise the concept of an array

--- DSA/8.Arrays_DS_1/Largest_and_Smallest_Numbers.java
+++ DSA/8.Arrays_DS_1/Largest_and_Smallest_Numbers.java
@@ -1,8 +1,8 @@
 public class Largest_and_Smallest_Numbers {
 
     public static int getLargest(int numbers[]) {
-        int largest = Integer.MIN_VALUE;
-        int smallest = Integer.MAX_VALUE;
+        int largest = Integer.MIN_VALUE; // -infinity
+        int smallest = Integer.MAX_VALUE; // +infinity
         for (int i = 0; i < numbers.length; i++) {
             if (largest < numbers[i]) {
                 largest = numbers[i];
@@ -11,12 +11,12 @@ public class Largest_and_Smallest_Numbers {
                 smallest = numbers[i];
             }
         }
-        System.out.println("smallest value is : " + smallest);
+        System.out.println("Smallest value is : " + smallest);
         return largest;
     }
 
     public static void main(String args[]) {
         int numbers[] = { 1, 2, 6, 3, 5 };
-        System.out.println("largest value is : " + getLargest(numbers));
+        System.out.println("Largest value is : " + getLargest(numbers));
     }
 }
