f820590140ba72bd4e4ade6bcf3c1f5867face51
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-04-25 10:31:29 +0530

    Added Files

--- DSA/9.Basic_Sorting_Algorithm/bubble_sort.java
+++ DSA/9.Basic_Sorting_Algorithm/bubble_sort.java
@@ -1,5 +1,5 @@
 public class bubble_sort {
-    public static void bubbleSort(int arr[]) {
+    public static void bubbleSort(int[] arr) {
         for(int i=0; i<arr.length - 1; i++) {
             for(int j=0; j<arr.length - 1 - i; j++) {
                 if(arr[j] > arr[j+1]) {
@@ -10,14 +10,14 @@ public class bubble_sort {
             }
         }
     }
-    public static void printArr(int arr[]) {
-        for(int i=0; i<arr.length; i++) {
-            System.out.print(arr[i] + " ");
+    public static void printArr(int[] arr) {
+        for (int j : arr) {
+            System.out.print(j + " ");
         }
         System.out.println();
     }
     public static void main(String[] args) {
-        int arr[] = {5, 4, 3, 2, 1};
+        int[] arr = {5, 4, 3, 2, 1};
         bubbleSort(arr);
         printArr(arr);
     }
