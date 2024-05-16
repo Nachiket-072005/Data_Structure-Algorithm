5bfdeb45822c6170ba01c68899acf2daa2683e98
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-05-16 19:37:48 +0530

    Selection_Sort Algorithm

--- DSA/9.Basic_Sorting_Algorithm/selection_sort.java
+++ DSA/9.Basic_Sorting_Algorithm/selection_sort.java
@@ -1,12 +1,13 @@
 public class selection_sort {
-    public static void selectionSort(int arr[]) {
-        for(int i=0; i<arr.length-1; i++) {
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
             int minPos = i;
-            for(int j=i+1; j<arr.length; j++) {
-                if(arr[minPos] > arr[j]) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[minPos] > arr[j]) {
                     minPos = j;
                 }
             }
+            // Swap
             int temp = arr[minPos];
             arr[minPos] = arr[i];
             arr[i] = temp;
@@ -14,13 +15,14 @@ public class selection_sort {
     }
 
     public static void printArr(int arr[]) {
-        for(int i=0; i<arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
+
     public static void main(String[] args) {
-        int arr[] = {5, 4, 3, 2, 1};
+        int arr[] = { 5, 4, 3, 2, 1 };
         selectionSort(arr);
         printArr(arr);
     }
