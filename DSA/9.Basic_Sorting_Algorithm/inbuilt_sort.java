f61386d9f6794003ba5ee2e7668bdf7d2ce04d31
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-05-16 20:25:40 +0530

    Inbuilt Sorting

--- DSA/9.Basic_Sorting_Algorithm/inbuilt_sort.java
+++ DSA/9.Basic_Sorting_Algorithm/inbuilt_sort.java
@@ -1,13 +1,15 @@
 import java.util.*;
+
 public class inbuilt_sort {
     public static void printArr(Integer arr[]) {
-        for(int i=0; i<arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
+
     public static void main(String[] args) {
-        Integer arr[] = {5, 4, 1, 3, 2};
+        Integer arr[] = { 5, 4, 1, 3, 2 };
         // Arrays.sort(arr);
         // Arrays.sort(arr, 0, 3);
         // Arrays.sort(arr, Collections.reverseOrder());
