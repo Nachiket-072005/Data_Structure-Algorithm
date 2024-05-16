aea92fc197f43d3667f71f70986319375ec9db89
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-05-16 20:07:48 +0530

    Insertion_Sort Algorithm

--- DSA/9.Basic_Sorting_Algorithm/insertion_sort.java
+++ DSA/9.Basic_Sorting_Algorithm/insertion_sort.java
@@ -1,26 +1,27 @@
 public class insertion_sort {
-    public static void insertionSort(int arr[]){
-        for(int i=1; i<arr.length; i++) {
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
             int curr = arr[i];
-            int prev = i-1;
-            //finding out correct position to insert an element
+            int prev = i - 1;
+            // Finding out the correct pos to insert
             while (prev >= 0 && arr[prev] > curr) {
-                arr[prev+1] = arr[prev];
+                arr[prev + 1] = arr[prev];
                 prev--;
             }
-            //insert an element
-            arr[prev+1] = curr;
+            // Insertion
+            arr[prev + 1] = curr;
         }
     }
 
     public static void printArr(int arr[]) {
-        for(int i=0; i<arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
+
     public static void main(String[] args) {
-        int arr[] = {5, 4, 1, 3, 2};
+        int arr[] = { 5, 4, 1, 3, 2 };
         insertionSort(arr);
         printArr(arr);
     }
