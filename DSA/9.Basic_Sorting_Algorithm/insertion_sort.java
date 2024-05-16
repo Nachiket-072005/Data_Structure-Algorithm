25f206c23c91cd10fe14f64e8f7ad372208be059
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-05-16 20:18:13 +0530

    Insertion Sort Algorithm

--- DSA/9.Basic_Sorting_Algorithm/insertion_sort.java
+++ DSA/9.Basic_Sorting_Algorithm/insertion_sort.java
@@ -3,7 +3,7 @@ public class insertion_sort {
         for (int i = 1; i < arr.length; i++) {
             int curr = arr[i];
             int prev = i - 1;
-            // Finding out the correct pos to insert
+            // Finding out the correct pos to insert an element
             while (prev >= 0 && arr[prev] > curr) {
                 arr[prev + 1] = arr[prev];
                 prev--;
