3c76dc06742f10aa6982117ef908896410983c88
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-05-16 21:23:20 +0530

    Counting Sort Algorithms

--- DSA/9.Basic_Sorting_Algorithm/Practice_Questions/counting_sort_DESC.java
+++ DSA/9.Basic_Sorting_Algorithm/Practice_Questions/counting_sort_DESC.java
@@ -3,17 +3,18 @@ package Practice_Questions;
 public class counting_sort_DESC {
     public static void countingSort(int arr[]) {
         int largest = Integer.MIN_VALUE;
-        for(int i=0; i<arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             largest = Math.max(largest, arr[i]);
         }
 
-        int count[] = new int[largest+1];
-        for(int i=0; i<arr.length; i++) {
+        int count[] = new int[largest + 1];
+        for (int i = 0; i < arr.length; i++) {
             count[arr[i]]++;
         }
 
-        int j=0;
-        for(int i=count.length-1; i>=0; i--) {
+        // Sorting
+        int j = 0;
+        for (int i = count.length - 1; i >= 0; i--) {
             while (count[i] > 0) {
                 arr[j] = i;
                 j++;
@@ -23,7 +24,7 @@ public class counting_sort_DESC {
     }
 
     public static void printArr(int arr[]) {
-        for(int i=0; i<arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
--- DSA/9.Basic_Sorting_Algorithm/counting_sort.java
+++ DSA/9.Basic_Sorting_Algorithm/counting_sort.java
@@ -1,17 +1,18 @@
 public class counting_sort {
     public static void countingSort(int arr[]) {
         int largest = Integer.MIN_VALUE;
-        for(int i=0; i<arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             largest = Math.max(largest, arr[i]);
         }
 
         int count[] = new int[largest + 1];
-        for(int i=0; i<arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             count[arr[i]]++;
         }
-        //sorting
-        int j=0;
-        for(int i=0; i<count.length; i++) {
+
+        // Sorting
+        int j = 0;
+        for (int i = 0; i < arr.length; i++) {
             while (count[i] > 0) {
                 arr[j] = i;
                 j++;
@@ -21,13 +22,14 @@ public class counting_sort {
     }
 
     public static void printArr(int arr[]) {
-        for(int i=0; i<arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
+
     public static void main(String args[]) {
-        int arr[] = {1, 4, 1, 3, 2, 4, 3, 7};
+        int arr[] = { 1, 4, 1, 3, 2, 4, 3, 7 };
         countingSort(arr);
         printArr(arr);
     }
