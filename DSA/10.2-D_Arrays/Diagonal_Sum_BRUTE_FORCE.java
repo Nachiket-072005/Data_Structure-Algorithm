a1eee7b9227e00b4a61c9dc75b2efec0483fd57b
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-05-17 08:06:15 +0530

    Bruth Force & Optimize code for Diagonal Sum

--- DSA/10.2-D_Arrays/Diagonal_Sum_BRUTE_FORCE.java
+++ DSA/10.2-D_Arrays/Diagonal_Sum_BRUTE_FORCE.java
@@ -1,27 +1,28 @@
 public class Diagonal_Sum_BRUTE_FORCE {
-    //Time Complexity = O(n^2)
+    // Time Complexity = O(n^2)
     public static int diagonalSum(int matrix[][]) {
         int sum = 0;
 
-        for(int i=0; i<matrix.length; i++) {
-            for(int j=0; j<matrix[0].length; j++) {
-                //primary diagonal sum
-                if(i == j) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                // primary diagonal sum
+                if (i == j) {
                     sum += matrix[i][j];
                 }
-                //secondary diagonal sum 
-                else if((i+j) == matrix.length-1) {
+                // secondary diagonal sum
+                else if ((i + j) == matrix.length - 1) {
                     sum += matrix[i][j];
                 }
             }
         }
         return sum;
     }
+
     public static void main(String[] args) {
         int matrix[][] = { { 1, 2, 3, 4 },
-                           { 5, 6, 7, 8 },
-                           { 9, 10, 11, 12 },
-                           { 13, 14, 15, 16 } };
+                { 5, 6, 7, 8 },
+                { 9, 10, 11, 12 },
+                { 13, 14, 15, 16 } };
         int sum = diagonalSum(matrix);
         System.out.println("Sum is : " + sum);
     }
