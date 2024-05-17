c1ce023112763191fa97d816bec0e21792756d47
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-05-17 07:27:09 +0530

    Creation of 2D Array

--- DSA/10.2-D_Arrays/creation_of_2_D_arrays.java
+++ DSA/10.2-D_Arrays/creation_of_2_D_arrays.java
@@ -1,11 +1,10 @@
 import java.util.*;
-
 public class creation_of_2_D_arrays {
 
     public static boolean search(int matrix[][], int key) {
-        for(int i=0; i<matrix.length; i++) {
-            for(int j=0; j<matrix[0].length; j++) {
-                if(matrix[i][j] == key) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == key) {
                     System.out.println("Key is at index : " + "(" + i + "," + j + ")");
                     return true;
                 }
@@ -15,11 +14,12 @@ public class creation_of_2_D_arrays {
         return false;
     }
 
+    //Task-1
     public static void largest(int matrix[][]) {
         int largest = Integer.MIN_VALUE;
-        for(int i=0; i<matrix.length; i++) {
-            for(int j=0; j<matrix[0].length; j++) {
-                if(matrix[i][j] > largest) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] > largest) {
                     largest = matrix[i][j];
                 }
             }
@@ -27,30 +27,32 @@ public class creation_of_2_D_arrays {
         System.out.println("largest element in the matrix is : " + largest);
     }
 
+    //Task-2
     public static void smallest(int matrix[][]) {
         int smallest = Integer.MAX_VALUE;
-        for(int i=0; i<matrix.length; i++) {
-            for(int j=0; j<matrix[0].length; j++) {
-                if(matrix[i][j] < smallest) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] < smallest) {
                     smallest = matrix[i][j];
                 }
             }
         }
         System.out.println("smallest element in the matrix is : " + smallest);
     }
+
     public static void main(String[] args) {
         int matrix[][] = new int[3][3];
         int n = matrix.length, m = matrix[0].length;
         Scanner sc = new Scanner(System.in);
 
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 matrix[i][j] = sc.nextInt();
             }
         }
         sc.close();
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
@@ -59,5 +61,5 @@ public class creation_of_2_D_arrays {
         search(matrix, 4);
         largest(matrix);
         smallest(matrix);
-    }    
+    }
 }
