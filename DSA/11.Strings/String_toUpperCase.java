6ab8a5c15df2cc65a487cb5e06e8b63058101d79
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-05-19 09:29:05 +0530

    String to convert firts letter into upperCase

--- DSA/11.Strings/String_toUpperCase.java
+++ DSA/11.Strings/String_toUpperCase.java
@@ -5,19 +5,19 @@ public class String_toUpperCase {
         char ch = Character.toUpperCase(str.charAt(0));
         sb.append(ch);
 
-        for(int i=1; i<str.length(); i++) {
-            if(str.charAt(i) == ' ' && i<str.length()-1) {
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == ' ' && i < str.length() - 1) {
                 sb.append(str.charAt(i));
                 i++;
                 sb.append(Character.toUpperCase(str.charAt(i)));
-            }
-            else {
+            } else {
                 sb.append(str.charAt(i));
             }
         }
         return sb.toString();
     }
-     public static void main(String args[]) {
+
+    public static void main(String args[]) {
         String str = "hi, i am nachiket";
         System.out.println(convToUpperCase(str));
     }
