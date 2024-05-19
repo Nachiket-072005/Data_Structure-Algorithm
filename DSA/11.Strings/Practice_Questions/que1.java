e281923fa08e086e1dca2c0a8d9ee32a539276fc
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-05-19 09:44:24 +0530

    Count LowerCase Vowels occured in a string entered by the user

--- DSA/11.Strings/Practice_Questions/que1.java
+++ DSA/11.Strings/Practice_Questions/que1.java
@@ -3,14 +3,15 @@ import java.util.Scanner;
 public class que1 {
     public static int countToLowercase(String name) {
         int count = 0;
-        for(int i=0; i<name.length(); i++) {
+        for (int i = 0; i < name.length(); i++) {
             char ch = name.charAt(i);
-            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
                 count++;
             }
         }
         return count;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String name = sc.nextLine();
