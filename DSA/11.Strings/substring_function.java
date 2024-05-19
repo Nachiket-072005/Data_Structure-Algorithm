419d40e4baba8dbeaab6148c323d914a5e1527a5
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-05-19 08:46:58 +0530

    Substring_Function

--- DSA/11.Strings/substring_function.java
+++ DSA/11.Strings/substring_function.java
@@ -1,13 +1,16 @@
 public class substring_function {
+    // This is user defined method to get substring.
     public static String subString(String str, int si, int ei) {
         String subStr = "";
-        for(int i=si; i<ei; i++) {
-             subStr += str.charAt(i);
+        for (int i = si; i < ei; i++) {
+            subStr += str.charAt(i);
         }
         return subStr;
     }
+
     public static void main(String[] args) {
         String str = "HelloWorld";
+        // Inbuilt Property to get substring.
         System.out.println(str.substring(0, 5));
         // System.out.println(subString(str, 0, 5));
     }
