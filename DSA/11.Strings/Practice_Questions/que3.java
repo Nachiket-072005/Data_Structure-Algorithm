543b0d3b09ff628ba8906047f37bcfbeeb718f1e
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-05-19 09:48:02 +0530

    Practice Question 3 Output

--- DSA/11.Strings/Practice_Questions/que3.java
+++ DSA/11.Strings/Practice_Questions/que3.java
@@ -1,5 +1,6 @@
 public class que3 {
     public static void main(String[] args) {
+        // It will replace all the l letters by empty strings.
         String str = "ApnaCollege".replace("l", "");
         System.out.println(str);
     }
