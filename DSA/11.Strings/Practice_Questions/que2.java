e626dea4d776b7d2532e55024bd587aa2b15701a
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-05-19 09:46:18 +0530

    False True answer

--- DSA/11.Strings/Practice_Questions/que2.java
+++ DSA/11.Strings/Practice_Questions/que2.java
@@ -3,7 +3,7 @@ public class que2 {
         String str = "ShradhaDidi";
         String str1 = "ApnaCollege";
         String str2 = "ShradhaDidi";
-
+        // It will give answer false, true.
         System.out.println(str.equals(str1) + " " + str.equals(str2));
     }
 }
