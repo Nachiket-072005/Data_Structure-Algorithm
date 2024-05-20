15423a9f2f432648884adbf805a539e411b3f0f7
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-05-20 18:21:53 +0530

    Set ith Bit

--- DSA/12.Bit Manipulation/set_ith_bit.java
+++ DSA/12.Bit Manipulation/set_ith_bit.java
@@ -3,6 +3,7 @@ public class set_ith_bit {
         int bitMask = 1 << i;
         return n | bitMask;
     }
+
     public static void main(String[] args) {
         System.out.println(setIthBit(10, 2));
     }
