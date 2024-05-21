096a9255a663f3f7f841b582972478dac3b67c67
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-05-21 07:07:41 +0530

    Power of 2

--- DSA/12.Bit Manipulation/check_Power_Of_Two.java
+++ DSA/12.Bit Manipulation/check_Power_Of_Two.java
@@ -2,6 +2,7 @@ public class check_Power_Of_Two {
     public static boolean isPowerOfTwo(int n) {
         return (n & (n - 1)) == 0;
     }
+
     public static void main(String[] args) {
         System.out.println(isPowerOfTwo(8));
     }
