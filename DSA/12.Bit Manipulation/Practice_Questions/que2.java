4ad84843766c1fd17cd427d304e2e460a8e65d22
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-05-21 07:25:06 +0530

    Swap 2 Numbers

--- DSA/12.Bit Manipulation/Practice_Questions/que2.java
+++ DSA/12.Bit Manipulation/Practice_Questions/que2.java
@@ -2,10 +2,10 @@ public class que2 {
     public static void main(String args[]) {
         int x = 4, y = 5;
         System.out.println("Before Swap : " + x + " and " + y);
-        //Swap using XOR
+        // Swap using XOR
         x = x ^ y;
         y = x ^ y;
         x = x ^ y;
-        System.out.println("After Swap : " + x + " and " + y); 
+        System.out.println("After Swap : " + x + " and " + y);
     }
 }
