f6a34afa974725b3bbc594886d026d7f37d31f8a
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-05-21 07:30:16 +0530

    Add 1 to an integer number

--- DSA/12.Bit Manipulation/Practice_Questions/que3.java
+++ DSA/12.Bit Manipulation/Practice_Questions/que3.java
@@ -6,5 +6,11 @@ public class que3 {
         System.out.println(x + "+" + 1 + " is " + -~x);
         x = 0;
         System.out.println(x + "+" + 1 + " is " + -~x);
+        /*
+         * We know that to get negative number, invert its bits and add 1 to
+         * it.(Remember negative number are stored in 2's complement form)
+         * -x = ~x + 1;
+         * -~x = x + 1;
+         */
     }
 }
