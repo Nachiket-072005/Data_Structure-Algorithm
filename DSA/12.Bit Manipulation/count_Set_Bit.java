54892351b11fa2183a8504fc120dfce80e9b4881
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-05-21 07:13:02 +0530

    Count Set Bits

--- DSA/12.Bit Manipulation/count_Set_Bit.java
+++ DSA/12.Bit Manipulation/count_Set_Bit.java
@@ -2,13 +2,15 @@ public class count_Set_Bit {
     public static int countSetBit(int n) {
         int count = 0;
         while (n > 0) {
-            if((n & 1) != 0) {
+            // To check our LSB
+            if ((n & 1) != 0) {
                 count++;
             }
             n = n >> 1;
         }
         return count;
     }
+
     public static void main(String[] args) {
         System.out.println(countSetBit(10));
     }
