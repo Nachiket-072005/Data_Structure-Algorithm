e552abf0b5ae75e2f6970f8829096a1f81de24f5
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-05-21 07:03:39 +0530

    Clear Range of ith Bit

--- DSA/12.Bit Manipulation/clear_range_of_bits.java
+++ DSA/12.Bit Manipulation/clear_range_of_bits.java
@@ -1,10 +1,11 @@
 public class clear_range_of_bits {
     public static int clearBitsInRange(int n, int i, int j) {
-        int a = ((~0) << (j+1));
+        int a = ((~0) << (j + 1));
         int b = (1 << i) - 1;
         int bitMask = a | b;
         return n & bitMask;
     }
+
     public static void main(String[] args) {
         System.out.println(clearBitsInRange(10, 2, 4));
     }
