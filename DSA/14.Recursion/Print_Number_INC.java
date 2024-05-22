93920ce621631faf281a92cc282ccd6f3a45c965
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-05-22 07:42:00 +0530

    Print Number Increasng Order by RECURSION

--- DSA/14.Recursion/Print_Number_INC.java
+++ DSA/14.Recursion/Print_Number_INC.java
@@ -1,12 +1,13 @@
 public class Print_Number_INC {
     public static void printInc(int n) {
-        if(n == 1) {
+        if (n == 1) {
             System.out.print(n + " ");
             return;
         }
         printInc(n - 1);
         System.out.print(n + " ");
     }
+
     public static void main(String[] args) {
         int n = 10;
         printInc(n);
