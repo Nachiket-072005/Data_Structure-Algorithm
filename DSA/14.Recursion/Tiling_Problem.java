7358201390dac116eaa37e634d767cf48a47faae
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-05-22 10:54:40 +0530

    Tiling Problem

---
+++ DSA/14.Recursion/Tiling_Problem.java
@@ -0,0 +1,22 @@
+public class Tiling_Problem {
+    public static int tilingProblem(int n) { // 2 x n
+        // Base Case
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        // vertical tiles
+        int fnm1 = tilingProblem(n - 1);
+
+        // horizontal tiles
+        int fnm2 = tilingProblem(n - 2);
+
+        // total ways
+        int totTiles = fnm1 + fnm2;
+        return totTiles;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(tilingProblem(4));
+    }
+}
