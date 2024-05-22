06584c0f5e240a1e301c3a482d205bcff9c79828
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-05-22 09:04:49 +0530

    Print Array is Sorted or Not

--- DSA/14.Recursion/Print_Array_Sorted.java
+++ DSA/14.Recursion/Print_Array_Sorted.java
@@ -1,17 +1,19 @@
 public class Print_Array_Sorted {
     public static boolean isSorted(int arr[], int i) {
-        if(i == arr.length - 1) {
+        if (i == arr.length - 1) {
             return true;
         }
 
-        if(arr[i] > arr[i + 1]) {
+        if (arr[i] > arr[i + 1]) {
             return false;
         }
 
-        return isSorted(arr, i+1);
+        return isSorted(arr, i + 1);
     }
+
+    // TC & SC = O(n)
     public static void main(String[] args) {
-        int arr[] = {1, 2, 3, 4, 6, 5};
+        int arr[] = { 1, 2, 3, 4, 6, 5 };
         System.out.println(isSorted(arr, 0));
     }
 }
