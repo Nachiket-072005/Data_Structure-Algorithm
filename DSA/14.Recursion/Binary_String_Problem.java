f6e2547a1868045b8ade4e50d5901f70a8ffc3f4
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-05-22 23:29:04 +0530

    Binary String Problem

---
+++ DSA/14.Recursion/Binary_String_Problem.java
@@ -0,0 +1,18 @@
+public class Binary_String_Problem {
+    public static void printBinString(int n, int lastPlace, String str) {
+        // Base Case
+        if (n == 0) {
+            System.out.println(str);
+            return;
+        }
+
+        printBinString(n - 1, 0, str + "0");
+        if (lastPlace == 0) {
+            printBinString(n - 1, 1, str + "1");
+        }
+    }
+
+    public static void main(String[] args) {
+        printBinString(3, 0, "");
+    }
+}
