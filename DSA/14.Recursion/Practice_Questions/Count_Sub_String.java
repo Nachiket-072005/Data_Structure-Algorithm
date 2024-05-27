83aefc9dc367d0063eb7789fd2c1c39cf5b69cac
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-05-27 19:51:43 +0530

    Count Sub String

---
+++ DSA/14.Recursion/Practice_Questions/Count_Sub_String.java
@@ -0,0 +1,25 @@
+public class Count_Sub_String {
+    public static int countString(String str, int i, int j, int n) {
+        if (n == 1) {
+            return 1;
+        }
+
+        if (n <= 0) {
+            return 0;
+        }
+
+        int res = countString(str, i + 1, j, n - 1) + countString(str, i, j - 1, n - 1)
+                - countString(str, i + 1, j - 1, n - 2);
+
+        if (str.charAt(i) == str.charAt(j)) {
+            res++;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String str = "abcab";
+        int n = str.length();
+        System.out.println(countString(str, 0, n - 1, n));
+    }
+}
