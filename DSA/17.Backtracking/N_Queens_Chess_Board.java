8c50b92cef780fa10e685f2b155867f4128727d2
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-05-28 18:04:57 +0530

    N Queens Chess Board

---
+++ DSA/17.Backtracking/N_Queens_Chess_Board.java
@@ -0,0 +1,64 @@
+public class N_Queens_Chess_Board {
+    public static boolean isSafe(char[][] board, int row, int col) {
+        // Vertical Up
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        // Diagonal Left
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        // Diagonal Right
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void nQueens(char[][] board, int row) {
+        // Base Case
+        if (row == board.length) {
+            printBoard(board);
+            return;
+        }
+
+        for (int j = 0; j < board.length; j++) {
+            if (isSafe(board, row, j)) {
+                board[row][j] = 'Q';
+                nQueens(board, row + 1);
+                board[row][j] = 'X';
+            }
+        }
+    }
+
+    public static void printBoard(char[][] board) {
+        System.out.println("--------- Chess Board ---------");
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        char[][] board = new char[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = 'X';
+            }
+        }
+
+        nQueens(board, 0);
+    }
+}
