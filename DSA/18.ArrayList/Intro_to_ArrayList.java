3e4c94f9be69a006cc57b55a228b3f4e209f75ad
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-06-04 10:58:57 +0530

    Intro to ArrayList

---
+++ DSA/18.ArrayList/Intro_to_ArrayList.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Intro_to_ArrayList {
+    public static void main(String[] args) {
+        // Java Collection FrameWork
+        // className objName = new className();
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<String> list2 = new ArrayList<>();
+        ArrayList<Float> list3 = new ArrayList<>();
+    }
+}
