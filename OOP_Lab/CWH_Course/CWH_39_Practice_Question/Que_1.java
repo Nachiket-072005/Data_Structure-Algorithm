03625d551e38d61e96a0a2ae36f40235b11b9559
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-06-05 19:11:20 +0530

    Problem_1

---
+++ OOP_Lab/CWH_Course/CWH_39_Practice_Question/Que_1.java
@@ -0,0 +1,27 @@
+class Employee {
+    int salary;
+    String name;
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+public class Que_1 {
+        public static void main(String[] args) {
+            Employee e1 = new Employee();
+
+            e1.setName("Nachiket");
+            // e1.salary = 35000;
+            // System.out.println(e1.getSalary());
+            System.out.println(e1.getName());
+        }
+    }
