87053383bcb17d5d513b9e0d6f4792411cc1a32a
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-06-06 10:25:49 +0530

    Print Reverse ArrayList

---
+++ DSA/18.ArrayList/Print_Reverse_ArrayList.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Print_Reverse_ArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        // System.out.println(list.size());
+
+        // Print Reverse ArrayList
+        for (int i = list.size() - 1; i >= 0; i--) {
+            System.out.print(list.get(i) + " ");
+        }
+        System.out.println();
+    }
+}
