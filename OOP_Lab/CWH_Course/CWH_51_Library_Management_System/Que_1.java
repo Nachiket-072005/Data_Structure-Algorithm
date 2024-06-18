b8b0c24d79a97bccd4f324fbf77c0115cad6ce4c
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-06-18 19:00:31 +0530

    Library Management System Question

---
+++ OOP_Lab/CWH_Course/CWH_51_Library_Management_System/Que_1.java
@@ -0,0 +1,8 @@
+public class Que_1 {
+    public static void main(String[] args) {
+        // You have to implement a Library Using Java Class Library
+        // Method: addBook, issueBook, returnBook, showAvailableBooks
+        // Properties: Array to store the available books,
+        // Array to store the issued books
+    }
+}
