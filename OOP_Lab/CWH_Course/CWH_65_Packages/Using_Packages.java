6a10b272d2b2a31143f379511730a94e4fbf4a55
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-06-26 19:06:40 +0530

    Packages

---
+++ OOP_Lab/CWH_Course/CWH_65_Packages/Using_Packages.java
@@ -0,0 +1,8 @@
+import com.packages.ex_1.Ex_1;
+
+public class Using_Packages {
+    public static void main(String[] args) {
+        System.out.println("I am using the packages.");
+        Ex_1 n = new Ex_1();
+    }
+}
---
+++ OOP_Lab/CWH_Course/CWH_65_Packages/com/packages/ex_1/Ex_1.java
@@ -0,0 +1,7 @@
+package com.packages.ex_1;
+
+public class Ex_1 {
+    public static void main(String[] args) {
+        System.out.println("Hello...");
+    }
+}
