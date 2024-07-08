4184d83ae6a2e21ca9b30e4f93cc5a4ceff2ff2b
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-07-08 15:48:35 +0530

    Que_6 -> Cimpletd

---
+++ OOP_Lab/Random_Stuff/GTU_Summer-2024/Que_6.java
@@ -0,0 +1,8 @@
+public class Que_6 {
+    public static void main(String[] args) {
+        StringBuffer s1 = new StringBuffer("Complete");
+        s1.setCharAt(1, 'i');
+        s1.setCharAt(7, 'd');
+        System.out.println(s1);
+    }
+}
