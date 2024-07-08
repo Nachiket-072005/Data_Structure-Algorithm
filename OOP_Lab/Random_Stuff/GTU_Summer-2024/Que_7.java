dfd6014ad3456bb43518cba1af57490f26abf3fa
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-07-08 15:50:39 +0530

    Que_7 -> 24 and I

---
+++ OOP_Lab/Random_Stuff/GTU_Summer-2024/Que_7.java
@@ -0,0 +1,8 @@
+public class Que_7 {
+    public static void main(String[] args) {
+        int Integer = 24;
+        char String = 'I';
+        System.out.println(Integer);
+        System.out.println(String);
+    }
+}
